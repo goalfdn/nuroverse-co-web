972dcda2c7b8b69e09ba00505afa376ae670353b
Nikhil Kumar <nikhilfaips@gmail.com>
2024-09-03 11:00:58 -0500

    Added config for Netlify hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     configFile: 'tailwind.config.js'
-  }), react()]
+  }), react()],
+  output: "server",
+  adapter: netlify()
 });
